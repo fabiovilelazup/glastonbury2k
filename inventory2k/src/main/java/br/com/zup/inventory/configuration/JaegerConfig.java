ddf9c6a893469e748fa5b449214d53f9d768b827
Fábio Borges de Oliveira Vilela <fabio.vilela@zup.com.br>
2020-03-20 14:27:07 -0300

    Configuração para Report dos Span para Jaeger desde o Container de Inventory

--- inventory2k/src/main/java/br/com/zup/inventory/configuration/JaegerConfig.java
+++ inventory2k/src/main/java/br/com/zup/inventory/configuration/JaegerConfig.java
@@ -5,19 +5,26 @@ import org.springframework.context.annotation.Bean;
 import io.jaegertracing.Configuration;
 import io.jaegertracing.Configuration.ReporterConfiguration;
 import io.jaegertracing.Configuration.SamplerConfiguration;
+import io.jaegertracing.Configuration.SenderConfiguration;
 import io.opentracing.Tracer;
 
 @org.springframework.context.annotation.Configuration
+//TODO : Verificar questões de autoconfiguration, baseando nos arquivo de propriedades.
+//@AutoConfigureBefore(io.opentracing.contrib.spring.tracer.configuration.TracerAutoConfiguration.class)
+//@EnableConfigurationProperties(JaegerConfigurationProperties.class)
 public class JaegerConfig {
 
 	@Bean
 	public static Tracer getTracer() {
-		SamplerConfiguration samplerConfig = SamplerConfiguration.fromEnv()
-				.withType("const").withParam(1);
-		ReporterConfiguration reporterConfig = ReporterConfiguration.fromEnv()
-				.withLogSpans(true);
-		Configuration config = new Configuration("inventory").withSampler(samplerConfig)
-				.withReporter(reporterConfig);
+		SamplerConfiguration samplerConfig = SamplerConfiguration.fromEnv().withType("const").withParam(1);
+
+		SenderConfiguration senderConfiguration = SenderConfiguration.fromEnv().withAgentHost("jaeger")
+				.withAgentPort(6831);
+
+		ReporterConfiguration reporterConfig = ReporterConfiguration.fromEnv().withLogSpans(true)
+				.withSender(senderConfiguration);
+
+		Configuration config = new Configuration("inventory").withSampler(samplerConfig).withReporter(reporterConfig);
 		return config.getTracer();
 	}
 }
