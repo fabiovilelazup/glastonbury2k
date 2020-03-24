1cc5a0f0fe5bfe0b760b2704988b5ffe05c94e5b
Fabio Borges Oliveira Vilela <fabio.vilela@zup.com.br>
2020-03-24 16:47:23 -0300

    Exclusão de código desnecessário

--- inventory2k/src/main/java/br/com/zup/inventory/controller/InventoryApi.java
+++ inventory2k/src/main/java/br/com/zup/inventory/controller/InventoryApi.java
@@ -2,10 +2,6 @@ package br.com.zup.inventory.controller;
 
 import java.util.List;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-
 import br.com.zup.inventory.controller.request.BookRequest;
 import br.com.zup.inventory.controller.request.RestoreRequest;
 import br.com.zup.inventory.controller.response.ProductResponse;
@@ -13,12 +9,9 @@ import br.com.zup.inventory.service.exception.ServiceException;
 
 public interface InventoryApi {
 
-	@GetMapping("/products")
 	List<ProductResponse> products() throws ServiceException;
 
-	@PostMapping("/booking")
-	void booking(@RequestBody BookRequest request) throws ServiceException;
+	void booking(BookRequest request) throws ServiceException;
 
-	@PostMapping("/restore")
-	void restore(@RequestBody RestoreRequest request) throws ServiceException;
+	void restore(RestoreRequest request) throws ServiceException;
 }
